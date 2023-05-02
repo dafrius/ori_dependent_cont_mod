32d10979911178eea3a39fcfde4402c7ae43bf0f
dafrius <m.u.canoluk@gmail.com>
2023-05-02 13:07:24 +0200

    lol

--- Low_Hori_2afc_modification_final_french_size_stuff.py
+++ Low_Hori_2afc_modification_final_french_size_stuff.py
@@ -284,7 +284,11 @@ SimFrame = int(100/framelength) #100 ms
 LeadFrame = int(50/framelength) #50 ms
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 reduce=50
+=======
+color_text = float(exp_info['color_text']) # the color of the text depends on the settings we select initialy
+>>>>>>> 55c0abc79ec347c1c172655e393ab19c884231c2
 =======
 color_text = float(exp_info['color_text']) # the color of the text depends on the settings we select initialy
 >>>>>>> 55c0abc79ec347c1c172655e393ab19c884231c2
@@ -582,7 +586,11 @@ def block_break(block_no, timer, alltotal):
 
 win.flip()
 <<<<<<< HEAD
+<<<<<<< HEAD
 pracscont = np.geomspace(0.005,0.03,20)
+=======
+pracscont = np.geomspace(0.01,0.1,20)
+>>>>>>> 55c0abc79ec347c1c172655e393ab19c884231c2
 =======
 pracscont = np.geomspace(0.01,0.1,20)
 >>>>>>> 55c0abc79ec347c1c172655e393ab19c884231c2
